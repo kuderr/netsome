7322a4842abaa258496a631c5e9a78a3c78711a9
kuderr <dakudryavcev@gmail.com>
2024-04-17 21:11:42 +0300

    removed pools

--- netsome/pools/bgp.py
+++
@@ -1,6 +0,0 @@
-from netsome.pools import number
-from netsome.types import bgp
-
-
-class AsnPool(number.IntRangePool):
-    _ITEM_CLS = bgp.ASN
--- netsome/pools/vlans.py
+++
@@ -1,6 +0,0 @@
-from netsome.pools import number
-from netsome.types import vlans
-
-
-class VlanPool(number.IntRangePool):
-    _ITEM_CLS = vlans.VID
