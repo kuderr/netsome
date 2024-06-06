f0374bd9145a0c6ba1c52a6f921cf659260dde45
lep0n <rastorguevgena@mail.ru>
2024-06-07 01:41:13 +0300

    add methods for PortInterface

--- netsome/types/ports.py
+++ netsome/types/ports.py
@@ -1,6 +1,6 @@
 import re
 
-import constants as c
+from netsome import constants as c
 
 
 class PortInterface:
@@ -9,5 +9,26 @@ class PortInterface:
 
     @staticmethod
     def map_port(value):
-        for pattern in c.PORT_PATTERNS:
+        for port_type, pattern in c.PORT_PATTERNS.items():
             match = re.match(pattern, value, re.IGNORECASE)
+            if match:
+                return port_type, match.group("value")
+
+        raise ValueError("Port type doesn't supports")
+
+    def __str__(self):
+        return f"{self._type} {self._number}"
+
+    def __repr__(self):
+        return f"PortInterface(type={self._type}, number={self._number})"
+
+    def __eq__(self, other):
+        if not isinstance(other, PortInterface):
+            raise TypeError(
+                "Сomparison is available only between PortInterface objects"
+            )
+
+        if other._type != self._type:
+            raise TypeError("PortInterface types are different")
+
+        return other._number == self._number
