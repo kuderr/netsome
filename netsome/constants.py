fa8c2169ae46e3cd1fa835cf2e1751615bd6f26e
lep0n <rastorguevgena@mail.ru>
2024-06-07 19:46:58 +0300

    big improve

--- netsome/constants.py
+++ netsome/constants.py
@@ -1,4 +1,5 @@
 import enum
+import re
 
 
 class NUMERALSYSTEMS(enum.IntEnum):
--- netsome/types/ports.py
+++ netsome/types/ports.py
@@ -3,28 +3,44 @@ import re
 from netsome import constants as c
 
 
-class PortInterface:
-    def __init__(self, value: str):
-        self._type, self._number = self.map_port(value)
+class Interface:
+    def __init__(self, string: str):
+        (
+            self._full_name,
+            self._short_name,
+            self._value,
+        ) = self.map_port(string)
 
     @staticmethod
-    def map_port(value):
-        for port_type, pattern in c.PORT_PATTERNS.items():
-            match = re.match(pattern, value, re.IGNORECASE)
+    def map_port(string):
+        for (full_name, short_name), pattern in c.IFACE_PATTERNS.items():
+            match = re.match(pattern, string)
             if match:
-                return port_type, match.group("value")
+                return full_name, short_name, match.group("value")
 
         raise ValueError("Port type doesn't supports")
 
     def __str__(self):
-        return f"{self._type} {self._number}"
+        return f"{self._full_name}{self._value}"
 
     def __repr__(self):
-        return f"PortInterface(type={self._type}, number={self._number})"
+        return f'PortInterface("{str(self)}")'
 
     def __eq__(self, other):
-       return (
+        return (
             isinstance(other, self.__class__)
-            and (self._type == other._type)
-            and (self._number == other._number)
+            and (self._full_name == other._full_name)
+            and (self._value == other._value)
         )
+
+    @property
+    def full_name(self):
+        return self._full_name
+
+    @property
+    def short_name(self):
+        return self._short_name
+
+    @property
+    def value(self):
+        return self._value
