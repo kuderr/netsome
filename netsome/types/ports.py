3cb0c070ea609f872fa784926b56274ccc81fe32
kuderr <dakudryavcev@gmail.com>
2024-06-11 19:58:17 +0300

    lil stuff

--- netsome/types/ports.py
+++ netsome/types/ports.py
@@ -1,49 +1,57 @@
+import functools
 import re
 
 from netsome import constants as c
-import dataclasses
-
-
-class InterfaceValue:
-    # TODO: split list of vals to attrs
-    def __init__(self, string):
-        self.vals, self.sub_iface = self.parse_iface_val(string)
-
-    def __str__(self):
-        return (
-            f"{"/".join(self.vals)}.{self.sub_iface[0]}"
-            if self.sub_iface
-            else "/".join(self.vals)
-        )
-
-    @staticmethod
-    def parse_iface_val(string):
-        vals, *sub_iface = string.split(".")
-        return vals.split("/"), sub_iface
 
 
 class Interface:
     def __init__(self, string: str):
-        (
-            self._full_name,
-            self._short_name,
-            self._value,
-        ) = self.map_port(string)
+        self._type, self._value = self.map_port(string)
+        self._full_name, self._short_name = c.IFACE_TYPES[self._type.name].value
+
+        vals, sub_iface = self.parse_iface_val(self._value)
+        self._sub_iface = int(sub_iface[0]) if sub_iface else None
+
+        # FIXME
+        vals = map(int, vals)
+        *lhs, self.port = vals
+        if lhs:
+            *lhs, self.slot = lhs
+            if lhs:
+                self.chassis = lhs[0]
 
     @staticmethod
     def map_port(string):
-        for (full_name, short_name), pattern in c.IFACE_PATTERNS.items():
+        for type, pattern in c.IFACE_PATTERNS.items():
             match = re.match(pattern, string)
             if match:
-                return full_name, short_name, InterfaceValue(match.group("value"))
+                return type, match.group("value")
 
         raise ValueError("Port type doesn't supports")
 
-    def __str__(self):
+    @staticmethod
+    def parse_iface_val(string):
+        vals, *sub_iface = string.split(c.DELIMITERS.DOT)
+        return vals.split(c.DELIMITERS.SLASH), sub_iface
+
+    @property
+    def type(self):
+        return self._type
+
+    @property
+    def value(self):
+        return self._value
+
+    @functools.cached_property
+    def canonical_name(self):
         return f"{self._full_name}{self._value}"
 
+    @functools.cached_property
+    def abbreviated_name(self):
+        return f"{self._short_name}{self._value}"
+
     def __repr__(self):
-        return f'PortInterface("{str(self)}")'
+        return f'{self.__class__.__name__}("{self.canonical_name}")'
 
     def __eq__(self, other):
         return (
@@ -51,15 +59,3 @@ class Interface:
             and (self._full_name == other._full_name)
             and (self._value == other._value)
         )
-
-    @property
-    def full_name(self):
-        return self._full_name
-
-    @property
-    def short_name(self):
-        return self._short_name
-
-    @property
-    def value(self):
-        return self._value
